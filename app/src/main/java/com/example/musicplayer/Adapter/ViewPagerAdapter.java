cbf89720cab43facef819696fdd99dc83fa56bb6
Imtiaz1997 <imtiazeemel42@gmail.com>
2021-09-09 20:30:48 +0600

    Tablayout Design

--- app/src/main/java/com/example/musicplayer/Adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/example/musicplayer/Adapter/ViewPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.musicplayer.Adapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -36,7 +37,15 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
         return fragments.size();
     }
 
-//    ArrayList<Fragment> fragments;
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+//        return super.getPageTitle(position);
+        return titles.get(position);
+    }
+
+
+    //    ArrayList<Fragment> fragments;
 //    private  ArrayList<String> titles;
 //
 //    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
