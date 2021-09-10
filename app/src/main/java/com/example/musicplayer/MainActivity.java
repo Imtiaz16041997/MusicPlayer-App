963d7e5a44be8bdf5d5514ec199310bce50a1c8d
Imtiaz1997 <imtiazeemel42@gmail.com>
2021-09-10 22:52:57 +0600

    Read Songs From Phone

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -9,22 +9,30 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.musicplayer.Adapter.ViewPagerAdapter;
 import com.example.musicplayer.Fragments.AlbumFragment;
 import com.example.musicplayer.Fragments.SongsFragment;
+import com.example.musicplayer.Models.MusicFiles;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager;
     ViewPagerAdapter pageAdapter;
 
     public static final int REQUEST_CODE = 1;
-
+    ArrayList<MusicFiles> musicFiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +40,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         permission();
-        initViewpager();
+
     }
 
     private void permission() {
-        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED)
         {
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_CODE);
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
         }
         else
         {
-            Toast.makeText(this,"Permission Granted !",Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this,"Permission Granted !",Toast.LENGTH_SHORT).show();
+            musicFiles = getAllAudio(this);
+            initViewpager();
         }
     }
 
@@ -55,10 +65,11 @@ public class MainActivity extends AppCompatActivity {
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
                 Toast.makeText(this,"Permission Granted !",Toast.LENGTH_SHORT).show();
+                musicFiles = getAllAudio(this);
             }
             else
             {
-                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_CODE);
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
             }
         }
     }
@@ -81,6 +92,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public static ArrayList<MusicFiles> getAllAudio(Context context)
+    {
+        ArrayList<MusicFiles> tempAudioList = new ArrayList<>();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String [] projection = {
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA, //for Path
+                MediaStore.Audio.Media.ARTIST,
+
+        };
+
+        Cursor cursor = context.getContentResolver().query(uri, projection, null,null,null);
+
+        if(cursor != null)
+        {
+            while(cursor.moveToNext())
+            {
+                String album = cursor.getString(0);
+                String title = cursor.getString(1);
+                String duration = cursor.getString(2);
+                String path = cursor.getString(3);
+                String artist = cursor.getString(4);
+
+
+                MusicFiles musicFiles = new MusicFiles(path,title,artist,album,duration);
+                //take log.e for check
+                Log.e("Path : "+path, "Album : "+album);
+                tempAudioList.add(musicFiles);
+            }
+            cursor.close();
+        }
+        return tempAudioList;
+
+    }
 
 
 
